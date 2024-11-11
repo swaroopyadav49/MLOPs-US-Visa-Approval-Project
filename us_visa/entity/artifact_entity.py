3fe4f1a75467472182ea46ee7a6f7864590b66d8
swaroop <swaroopyadav49@gmail.com>
2024-11-11 22:51:48 +0530

    data validation updated

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -7,3 +7,10 @@ class DataIngestionArtifact:
     test_file_path:str 
 
 
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    drift_report_file_path: str
+
+
